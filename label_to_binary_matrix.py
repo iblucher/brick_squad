703afbb8efdab125faf5c93a6ed2ccc42f14bda6
Isabela Blucher <isabelablucher@hotmail.com>
2018-05-08 13:52:17 +0200

    Remove old file

--- label_to_binary_matrix.py
+++
@@ -1,8 +0,0 @@
-from sklearn.preprocessing import MultiLabelBinarizer
-
-def label_to_binary_matrix(labels):
-
-    # initialize multilabel bainrizer
-    mlb = MultiLabelBinarizer()
-    bmatrix = mlb.fit_transform(labels)
-    return bmatrix
